a6f081fb4d777c10d8fe76bf806a98dad29106db
Ali Mardan <77826162+mardan-shah@users.noreply.github.com>
2023-03-18 01:52:58 +0500

    New files with firebase connection

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -3,7 +3,6 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
-
 const Stars = (props) => {
   const ref = useRef();
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
@@ -15,7 +14,7 @@ const Stars = (props) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} position={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color='#f272c8'
