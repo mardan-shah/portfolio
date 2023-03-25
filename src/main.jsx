1377b84b8725401e9d7a6c303544f3693b275e35
Ali Mardan <77826162+mardan-shah@users.noreply.github.com>
2023-03-25 17:06:30 +0500

    Your commit message here

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDmMAaO08wRRL2OIqsePkyHK1q_6K8blOU",
+    authDomain: "portfolio-fa79f.firebaseapp.com",
+    projectId: "portfolio-fa79f",
+    storageBucket: "portfolio-fa79f.appspot.com",
+    messagingSenderId: "216049014952",
+    appId: "1:216049014952:web:700fbe0ef7789f3e844a9d",
+    measurementId: "G-71GJDJTGE6"
+};
+  
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
